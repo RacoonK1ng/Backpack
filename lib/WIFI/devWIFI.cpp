728f09c4db359b6119845c53c823c69629411ab6
RacoonK1ng <nevio09.caputo@icloud.com>
2025-11-02 11:58:37 +0100

    Added Webpages to build_html.py

--- lib/WIFI/devWIFI.cpp
+++ lib/WIFI/devWIFI.cpp
@@ -153,7 +153,7 @@ static bool captivePortal(AsyncWebServerRequest *request)
   {
     DBGLN("Request redirected to captive portal");
     request->redirect(String("http://") + toStringIp(request->client()->localIP()));
-    return true;
+    return true;<
   }
   return false;
 }
@@ -686,7 +686,7 @@ static void startServices()
   server.on("/mui.js", WebUpdateSendContent);
   server.on("/scan.js", WebUpdateSendContent);
   server.on("/logo.svg", WebUpdateSendContent);
-  server.on("/ELRS_drone_tracker/tracker.html", WebUpdateSendContent);
+  server.on("/ELRS_drone_tracker/tracker.html", WebUpdateHandleRoot);
   server.on("/ELRS_drone_tracker/tracker.css", WebUpdateSendContent);
   server.on("/ELRS_drone_tracker/tracker.js", WebUpdateSendContent);
   server.on("/config", HTTP_GET, GetConfiguration);
--- python/build_html.py
+++ python/build_html.py
@@ -60,6 +60,8 @@ def build_common(env, mainfile):
             build_html("p5.js", "P5_JS", out, env)
             build_html("airplane.obj", "PLANE_OBJ", out, env)
             build_binary("texture.gif", "TEXTURE_GIF", out, env)
+            build_html("ELRS_drone_tracker/tracker.css", "TRACKER_CSS", out, env)
+            build_html("ELRS_drone_tracker/tracker.css", "TRACKER_JS", out, env)
     finally:
         if not os.path.exists("include/WebContent.h") or not filecmp.cmp(path, "include/WebContent.h"):
             shutil.copyfile(path, "include/WebContent.h")
@@ -71,5 +73,7 @@ if fnmatch.filter(env['BUILD_FLAGS'], '*TARGET_VRX_BACKPACK*'):
     build_common(env, "vrx_index.html")
 elif fnmatch.filter(env['BUILD_FLAGS'], '*TARGET_TX_BACKPACK*'):
     build_common(env, "txbp_index.html")
+    build_common(env, "ELRS_drone_tracker/tracker.html")
 elif fnmatch.filter(env['BUILD_FLAGS'], '*TARGET_TIMER_BACKPACK*'):
     build_common(env, "timer_index.html")
+    build_common(env, "ELRS_drone_tracker/tracker.html")
